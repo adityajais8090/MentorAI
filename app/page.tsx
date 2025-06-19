eab901473a6d413178d503f75029812bdb0c2e34
adityajais8090 <a_jaiswal@ece.iitr.ac.in>
2025-06-19 18:05:52 +0530

    [added]  Clerk Auth and Companion Library

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,59 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+import CTA from '@/components/CTA';
+import { Button } from '@/components/ui/button';
+import { recentSessions } from '@/constants';
+import React from 'react';
+import { cn } from '@/lib/utils'; 
+
+
+
+
+
 
 const Page = () => {
   return (
-    <div>
-      <h1 className = "text-2xl underline"> Welcome to my Saas App </h1>
-      <Button>
-        Let's get started
-      </Button>
+    
+      <main>
+        <h2 className="text-3xl font-bold">Popular Companions</h2>
+        <section className="home-section">
+          <CompanionCard
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neural Network of the Brain"
+            subject="science"
+            duration={45}
+            color="#ffda6e"
+          />
+          <CompanionCard
+            id="456"
+            name="Wizard Number Magic"
+            topic="Derivatives and Integrals"
+            subject="math"
+            duration={45}
+            color="#e5d0ff"
+          />
+          <CompanionCard
+            id="789"
+            name="Verba the Vocabulary Builder"
+            topic="Language"
+            subject="english"
+            duration={45}
+            color="#ffda6e"
+          />
+        </section>
 
-    </div>
-  )
-}
+        <section className="home-section">
+          <CompanionsList
+            title="Recently Completed Session"
+            companions={recentSessions}
+            classNames="w-2/3 max-lg:w-full"
+          />
+          <CTA />
+        </section>
+      </main>
+    
+  );
+};
 
-export default Page
+export default Page;
