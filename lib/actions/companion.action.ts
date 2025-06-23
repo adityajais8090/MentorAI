4099b4a7a8a6a73474209bd9a4da486cee9d80be
adityajais8090 <a_jaiswal@ece.iitr.ac.in>
2025-06-23 08:52:51 +0530

    [add] Companion page, [add] user Companion Creation Limit, [add] Subscription Upgrade Page

--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -102,12 +102,40 @@ export const getUserCompanions = async ( userId : string) => {
     const supabase = createSupabaseClient();
     const { data, error } = await supabase
             .from('companions')
-            .select('companions: companion_id (*)')
+            .select()
             .eq('author', userId )
             
-            
-
     if(error) throw new Error(error.message);
 
     return data;
+}
+
+
+export const newCompanionPermissions = async() => {
+         const {userId, has} = await auth();
+         const supabase = createSupabaseClient();
+         
+         let limit = 0;
+         if(has({ plan : 'pro'})){
+            return true;
+         }else if(has({ feature : "3_companion_limit"})){
+            limit = 3;
+         }else if(has({ feature : "10_companion_limit"})){
+            limit = 3;
+         }
+
+         const {data, error} = await supabase
+            .from('companions')
+            .select('id', {count : 'exact'})
+            .eq('author', userId)
+
+         if(error) throw new Error(error.message)
+            
+         const companionCount = data?.length;
+         
+         if(companionCount >= limit) { 
+            return false;
+         }else{
+            return true;
+         }
 }
